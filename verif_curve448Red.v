17045cbba5f2e90c70d445aab0143381f0953eef
david-hrnndz <102129342+david-hrnndz@users.noreply.github.com>
2022-07-21 13:49:39 -0400

    progress in Red

--- verif_curve448Red.v
+++ verif_curve448Red.v
@@ -13,8 +13,6 @@ Local Open Scope Z.
 
 Definition prime: Z := 2^448 - 2^224 - 1.
 
-Compute prime. 
-
 Definition curve448Red_spec : ident * funspec :=
 DECLARE _curve448Red
 WITH    r : val,
@@ -38,19 +36,30 @@ POST [ tvoid ]
    (*cont*)(map Vint (map Int.repr (int_to_list ((list_to_int contents_a) mod prime)))) r ;
             data_at Tsh (tarray tuint 14) (map Vint (map Int.repr contents_a)) a).
 
-Definition curve448Red_INV_1    (r : val)  shr (contents_r : list val) 
-                                (a : val)  (contents_a : list Z) (h : Z) 
-                                (t : val)  
-                                (b : )
+Definition curve448Red_INV_1    (a : val)  (contents_a : list Z) 
+                                (b : val) (contents_b : list val) (shb : share)
+                                (h : Z) (gv : globals) 
 := 
     (EX i : Z,
-    (PROP   (writable_share shr; 
-            Zlength contents_r = 14;
-            Zlength contents_a = 14)
-    LOCAL   (temp _r r ; temp _temp t ; temp _b b)
-    SEP     (data_at shr (tarray tuint 14) 
+    (PROP   (Zlength contents_a = 14;
+            writable_share shb)
+    LOCAL   (temp _temp (Vlong (Int64.repr (Int.signed (Int.repr 1))));
+            lvar _b (tarray tuint 14) b; gvars gv; 
+            temp _a a; temp _h (Vint (Int.repr h)))
+    SEP     (data_at shb (tarray tuint 14) 
             (map Vint (map Int.repr contents_a)) a
             )))%assert.
                 
+Definition Gprog : funspecs := ltac:(with_library prog [ curve448Red_spec ]).
+
+Lemma body_curve448Red : semax_body Vprog Gprog f_curve448Red curve448Red_spec.
+Proof.
+    start_function.
+    forward.
+    forward.
+    forward_for_simple_bound 7 
+        (curve448Red_INV_1 a contents_a v_b contents_b shb h gv).  
+
+
 
 
