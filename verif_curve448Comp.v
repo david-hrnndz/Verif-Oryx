a1d1248a4dbab489abada60ae96adf44c57e849e
Jaziel Torres <jaziel.torres@upr.edu>
2022-07-29 09:00:44 -0400

    Verification completed!

--- verif_curve448Comp.v
+++ verif_curve448Comp.v
@@ -3,13 +3,82 @@ Require Import curve448.
 Require Import stdpp.list.
 Require Import ZArith.
 Require Import compcert.lib.Coqlib.
+Require Import int_lemmas list_int_functions.
 
 Instance CompSpecs : compspecs. Proof. make_compspecs prog. Defined.
 Definition Vprog : varspecs.  mk_varspecs prog. Defined.
 
 Local Open Scope Z.
 
-Scheme Equality for list.
+(* The same mask that is computed in the body of curve448Comp *)
+Fixpoint mask (a b : list Z) : int := 
+    match a with
+    | [] => Int.zero
+    | a0 :: a => match b with
+        | [] => Int.zero
+        | b0 :: b => Int.or (Int.xor (Int.repr a0) (Int.repr b0)) (mask a b)
+        end
+    end.
+
+
+Lemma mask_app (a1 a2 b1 b2 : list Z) : Zlength a1 = Zlength b1 ->
+mask (a1 ++ a2) (b1 ++ b2) =
+Int.or (mask a1 b1) (mask a2 b2).
+Proof.
+    generalize dependent b1.
+    induction a1.
+    - intros.
+      rewrite Zlength_nil in H; symmetry in H.
+      apply Zlength_nil_inv in H.
+      rewrite H.
+      simpl; rewrite Int.or_zero_l; auto.
+    - destruct b1; intros.
+      + rewrite Zlength_nil in H; apply Zlength_nil_inv in H.
+        rewrite H; simpl; rewrite Int.or_zero_l; auto.
+      + simpl.
+        rewrite IHa1, Int.or_assoc; auto.
+        rewrite !Zlength_cons in H.
+        apply (f_equal Z.pred) in H;
+        rewrite <- !Zpred_succ in H.
+        assumption.
+Qed.
+
+Lemma list_Inteq_mask0 : forall a b : list Z, 
+    list_Inteq a b = true -> mask a b = Int.zero.
+Proof.
+    induction a as [|a0 a]; intros; auto.
+    destruct b as [|b0 b]; simpl in H; try discriminate.
+    apply andb_prop in H; destruct H.
+    pose proof (Int.eq_spec (Int.repr a0) (Int.repr b0)).
+    rewrite H in H1.
+    unfold mask.
+    rewrite H1, Int.xor_idem, Int.or_zero_l.
+    fold mask; rewrite IHa; auto.
+Qed.
+
+Lemma mask0_list_Inteq : forall a b : list Z, Zlength a = Zlength b -> 
+    mask a b = Int.zero -> list_Inteq a b = true.
+Proof.
+    induction a as [ | a0 a]; intros.
+    -   rewrite Zlength_nil in H.
+        symmetry in H; apply Zlength_nil_inv in H.
+        rewrite H; auto.
+    -   destruct b as [|b0 b].
+        +   rewrite Zlength_nil in H; apply Zlength_nil_inv in H.
+            rewrite H; auto.
+        +   simpl. 
+            simpl in H0.
+            apply or_eq0 in H0; destruct H0; auto.
+            apply Int.xor_zero_equal in H0.
+            rewrite H0.
+            rewrite Int.eq_true, IHa; simpl; auto.
+            rewrite !Zlength_cons in H.
+            apply (f_equal Z.pred) in H;
+            rewrite <- !Zpred_succ in H; auto.
+Qed.
+
+
+(* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% *)
 
 Definition curve448Comp_spec : ident * funspec :=
 DECLARE _curve448Comp
@@ -24,33 +93,22 @@ PRE [ tptr tuint, tptr tuint ]
             data_at Tsh (tarray tuint 14) (map Vint (map Int.repr contents_b)) b)
 POST [ tuint ]
     PROP   ()
-    RETURN (if (list_beq Z Z.eqb contents_a contents_b) then Vint Int.zero else Vint Int.one)
+    RETURN (if (list_Inteq contents_a contents_b) then Vint Int.zero else Vint Int.one)
     SEP    (data_at Tsh (tarray tuint 14) (map Vint (map Int.repr contents_a)) a;
             data_at Tsh (tarray tuint 14) (map Vint (map Int.repr contents_b)) b).
-
-
-Definition Comp (i : Z) (a b : list Z) : int := 
-    if (list_beq Z Z.eqb (sublist.sublist 0 i a) (sublist.sublist 0 i b))
-    then Int.zero else Int.one.
-
-
-
-(* AQUIIIIIIIIIIIIIIIIII *)
-
-Lemma L : list_beq Z Z.eqb (sublist.sublist 0 (i + 1) l1)
-(sublist.sublist 0 (i + 1) l2) = true ->
-list_beq Z Z.eqb (sublist.sublist 0 i l1) (sublist.sublist 0 i l2) = true /\ 
-Zinth i l1 = Zinth i l2.
 
 
 Definition curve448Comp_INV a contents_a b contents_b := 
 (EX i : Z,
 (PROP   (Zlength contents_a = 14;
          Zlength contents_b = 14)
-LOCAL   (temp _a a; temp _b b; temp _mask (Vint (Comp i contents_a contents_b)))
+LOCAL   (temp _a a; temp _b b; temp _mask (Vint (mask 
+         (sublist.sublist 0 i contents_a) 
+         (sublist.sublist 0 i contents_b))))
 SEP     (data_at Tsh (tarray tuint 14) (map Vint (map Int.repr contents_a)) a;
          data_at Tsh (tarray tuint 14) (map Vint (map Int.repr contents_b)) b
         )))%assert.
+
 
 Definition Gprog : funspecs := ltac:(with_library prog [ curve448Comp_spec ]).
 
@@ -64,51 +122,36 @@ Proof.
     -   repeat forward.
         entailer!.
         apply f_equal.
-        unfold Comp.
-        destruct (list_beq Z Z.eqb (sublist.sublist 0 (i + 1) contents_a)
-        (sublist.sublist 0 (i + 1) contents_b)) eqn:E.
-        unfold list_beq in E.0
-        inversion E.
-        Search "lxor" inside Z.
-
-        simpl.
-        replace (0 - 1) with (-1) by lia.
-        rewrite Z.land_m1_r.
-        replace (Int.not (Int.repr (-1))) with (Int.zero) by easy.
-        rewrite Int.and_zero, Int.or_zero.
-        autorewrite with sublist.
-        replace (sublist.sublist 0 i (map Vint (map Int.repr contents_a)) ++
-        upd_Znth 0 (sublist.sublist i 14 contents_r)
-        (Vint (Int.repr (Znth i contents_a))))
-        with  
-        (sublist.sublist 0 (i + 1) (map Vint (map Int.repr contents_a)) ++
-        sublist.sublist (i + 1) 14 contents_r)
-        by list_simplify; cancel.
-    -   entailer!.
-        simpl. 
-        autorewrite with sublist; cancel.
-    * (* Case c = 0 *)
-    forward.
-    forward_for_simple_bound 14 
-    (curve448Comp_INV r shr contents_r a contents_a b contents_b c);
-    rewrite H2; autorewrite with norm.
-    -   entailer!.
-        simpl; autorewrite with sublist; cancel.
-    -   repeat forward. 
+        rewrite <- !(sublist_rejoin 0 i (i+1)); try split; try lia.
+        rewrite mask_app, !sublist_len_1; simpl.
+        rewrite Int.or_zero; auto.
+        rewrite H0; auto.
+        rewrite H; auto.
+        list_simplify.
+    -   forward.   
         entailer!.
-        simpl.
-        replace (1 - 1) with 0 by lia.
-        rewrite Z.land_0_r.
-        unfold Int.not.
-        rewrite Int.xor_zero_l, Int.and_mone, Int.or_zero_l.
-        replace (upd_Znth i
-        (sublist.sublist 0 i (map Vint (map Int.repr contents_b)) ++
-         sublist.sublist i 14 contents_r) (Vint (Int.repr (Znth i contents_b))))
-        with 
-        (sublist.sublist 0 (i + 1) (map Vint (map Int.repr contents_b)) ++
-        sublist.sublist (i + 1) 14 contents_r)
-        by list_simplify; cancel.
-    -   entailer!.
-        simpl.
-        autorewrite with sublist; cancel.   
+        rewrite <- H at 1 3; rewrite <- H0.
+        replace (sublist.sublist 0 (Zlength contents_a) contents_a) with contents_a
+        by list_simplify.
+        replace (sublist.sublist 0 (Zlength contents_b) contents_b) with contents_b
+        by list_simplify.
+        destruct (list_Inteq contents_a contents_b) eqn:E; f_equal.
+        + apply list_Inteq_mask0 in E.
+        rewrite E, Int.or_zero_l, Int.not_zero.
+        unfold Int.add.
+        replace (Int.repr 1) with Int.one by auto.
+        rewrite Int.unsigned_one, Int.unsigned_mone.
+        replace (Int.modulus - 1 + 1) with Int.modulus by rep_lia.
+        rewrite Int.shru_lt_zero; auto.
+        + remember (mask contents_a contents_b) as x.
+        rewrite <- Int.or_shru, <- Int.neg_not,
+        !Int.shru_lt_zero.
+        destruct (Int.lt x Int.zero) eqn:E1;
+        destruct (Int.lt (Int.neg x) Int.zero) eqn:E2; auto.
+        pose proof (lt_false_eq0 x E1 E2).
+        rewrite Heqx in H7.
+        assert (Heq : Zlength contents_a = Zlength contents_b).
+            rewrite H, H0; auto.
+        apply (mask0_list_Inteq contents_a contents_b Heq) in H7.
+        rewrite H7 in E; discriminate.
 Qed.
